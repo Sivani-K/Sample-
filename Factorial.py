ee6e8f098241545fc365b092b7712e0463df5407
Sivani-K <sivanikovalan07@gmail.com>
2026-01-12 22:58:59 +0530

    Create Factorial.py

---
+++ Factorial.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+num = 5
+print("Factorial of", num, "is", factorial(num))
+
+# Expected output:
+# Factorial of 5 is 120
